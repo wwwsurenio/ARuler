94e556cafbdb9ac2a42a21f813d60e196092d454
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-03-29 15:15:07 +0200

    added result display in app

--- ARuler/ViewController.swift
+++ ARuler/ViewController.swift
@@ -91,7 +91,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let distance = sqrt( pow(a, 2) + pow(b, 2) + pow(c, 2) )
         
-        print(abs(distance))
+        updateText(text: "\(abs(distance))", atPosition: end.position)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3){
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.white
+        
+        let textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
     }
     
 } //end of class
